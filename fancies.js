a5306d24eafe5683ea890f4de6734e400681cf49
Emma <EmmaLaine@hotmail.com>
2018-04-10 14:38:48 -0500

    Add files via upload

---
+++ fancies.js
@@ -0,0 +1,10 @@
+//responsive topnav
+	function responsiveNav() {
+		var x = document.getElementById("myTopnav");
+		if (x.className === "topnav") {
+			x.className += " responsive";
+		} else {
+			x.className = "topnav";
+		}
+	};
+
